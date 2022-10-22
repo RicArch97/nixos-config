fd6a7e6bb78d62253fa5cd6909953e00c5a71bb6
Ricardo Steijn <ricardo.steijn97@gmail.com>
2022-10-23 01:45:51 +0200

    hosts/T470: increase file descriptors for installation

--- hosts/T470/default.nix
+++ hosts/T470/default.nix
@@ -33,6 +33,9 @@ Should not contain any gaming related stuff.
     ];
   };
 
+  # Increase file descriptors, installer complains that there aren't enough
+  systemd.extraConfig = "DefaultLimitNOFILE=1048576";
+
   # thinkpad trackpoint
   hardware.trackpoint.enable = true;
   hardware.trackpoint.emulateWheel = true;
