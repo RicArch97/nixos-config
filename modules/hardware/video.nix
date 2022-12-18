1ef3618f9d1aa93477b8e7685bf309a0285d75b4
Ricardo Steijn <ricardo.steijn97@gmail.com>
2022-12-18 01:19:03 +0100

    modules/hardware: don't install amdvlk for amd hardware

--- modules/hardware/video.nix
+++ modules/hardware/video.nix
@@ -37,16 +37,11 @@ in {
       boot.initrd.kernelModules = ["amdgpu"];
       services.xserver.videoDrivers = ["amdgpu"];
 
-      # enables AMDVLK & OpenCL support
+      # enables OpenCL support
       hardware.opengl.extraPackages = [
-        pkgs.amdvlk
         pkgs.rocm-opencl-icd
         pkgs.rocm-opencl-runtime
       ];
-      hardware.opengl.extraPackages32 = [pkgs.driversi686Linux.amdvlk];
-
-      # force use of RADV, can be unset if AMDVLK should be used
-      environment.variables.AMD_VULKAN_ICD = "RADV";
     })
 
     (lib.mkIf (device.gpu == "intel") {
